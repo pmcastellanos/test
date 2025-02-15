b151fd9dfab2a48ff847e4657d6df0ff8840b30d
pcastesa <pcastesa@emeal.nttdata.com>
2025-02-15 09:55:15 +0100

    primer commit

---
+++ src/test/java/com/maven/artefacto/NombreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maven.artefacto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NombreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
